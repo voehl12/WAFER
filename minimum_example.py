fac8625ff85e77be2e0fa407465dd55970bba1b0
voehl12 <veronika.oehl@gmx.de>
2022-09-14 16:48:30 +0200

    Comments and Structure

--- minimum_example.py
+++ minimum_example.py
@@ -7,12 +7,16 @@ import scipy
 from utils import wavelets,plotting, funcs
 from SFM import SFM,SFM_BSpline
 from scipy import optimize
+from datetime import datetime
 
 
 
+################### retrieval parameters ###############################
+
+directory = '../cwavelets/libradtranscope/floxseries_ae_oen/brdf/'
 
 mineval,maxeval,eval_wl = 754,773,760
-polyorder = 2
+
 
 cab = 20
 lai = 3
@@ -22,11 +26,19 @@ testjmin = -2.5
 testjmax = 3
 testnlevels = 2048
 noise = 0
+polyorder = 2
 
-wl, upsignal, whitereference, refl, F,noF = prepare_input.synthetic(cab,lai,fe,wlmin=mineval,wlmax=maxeval,completedir='../cwavelets/libradtranscope/floxseries_ae_oen/brdf/')
+########################################################################
+
+
+wl, upsignal, whitereference, refl, F,noF = prepare_input.synthetic(cab,lai,fe,wlmin=mineval,wlmax=maxeval,completedir=directory)
 
 refnoise, whitereference = prepare_input.add_noise(whitereference,1000,noise)
 sensornoise, upsignal = prepare_input.add_noise(upsignal,1000,noise)
+
+############### initial guess and reflectance optimization##############
+
+start = datetime.now()
 appref = np.divide(upsignal,whitereference)
 nopeak_wl, nopeak_appref = prepare_input.rm_peak(wl,appref) 
 
@@ -39,10 +51,13 @@ newdecomp = wavelets.decomp(testjmin,testjmax,testnlevels)
 newdecomp.adjust_levels(upsignal)
 newdecomp.create_comps(upsignal)
 
-weights = wavelets.determine_weights(upsignal,newdecomp.scales)
-
 coeffs,ress = rpoly.optimize_coeffs(wl,whitereference,upsignal,p_init,newdecomp)
 
+########################################################################
+
+############# weighting and final reflectance ##########################
+newdecomp.calc_weights(upsignal)
+weights = newdecomp.weights
 coeffs = np.array(coeffs)
 polyrefls = []
            
@@ -51,14 +66,18 @@ for j,polycoef in enumerate(coeffs):
     interp = np.poly1d(polycoef)
     polyrefls.append(interp(wl))
     
-
-        
-        
 polyrefls = np.array(polyrefls)
 polyR = np.ma.average(polyrefls,weights=weights,axis=0)
 R_std = funcs.weighted_std(polyrefls,weights=weights,axis=0)
 
+########################################################################
+
+
 F_der = upsignal-polyR*whitereference
+
+end = datetime.now()
+print(end-start)
+
 F_err = np.sqrt(np.square(sensornoise) + np.square(np.multiply(whitereference,R_std)) + np.square(np.multiply(polyR,refnoise)))
                 
 plt.figure()
