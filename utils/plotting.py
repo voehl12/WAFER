9a361cdb9c474fad50ad5aa14ae0b57acb651e70
voehl12 <veronika.oehl@gmx.de>
2022-09-16 14:22:22 +0200

    Clean up

--- utils/plotting.py
+++ utils/plotting.py
@@ -24,14 +24,7 @@ def plot_powerspectrum(wl,spectrum,scales,wlscales,filename):
     figF.canvas.draw()
     #axF.plot(wl,signal/np.max(signal)*30,color='black',linewidth=3)
     #labels = [int(i.get_position()[1]) for i in ax2.get_yticklabels()]
-    
-    """ labels_new = np.zeros(len(labels))
-    for j,i in enumerate(labels):
-        if i >= 0:
-            labels_new[j] = np.round(wlscales[i],2)
-
-    ax2.set_yticklabels(labels_new) """
-    
+      
     ax2.set_ylabel(r'peak width [nm]')
     #rect = patches.Rectangle((wl[1], 29), wl[-2]-wl[1], 1, linewidth=1, edgecolor='r', facecolor='none')
     #axF.add_patch(rect)
@@ -46,19 +39,13 @@ def get_colormap(N,m='viridis'):
 
 
 def plot_3d(x,y,data,name,text):
-    #fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+    
     fig, ax = plt.subplots()
     ax.text(x[0]-0.2,y[0]-1,text,color='white')
 
-    # Make data.
-    
     x, y = np.meshgrid(x, y)
     pl = ax.pcolormesh(x, y, data,shading='auto',cmap='magma_r',vmin=0,vmax=1.0)
-    #surf = ax.plot_surface(x, y, data, cmap=cm.magma,linewidth=0, antialiased=False)
 
-    
-
-    # Add a color bar which maps values to colors.
     cb = fig.colorbar(pl)
     cb.ax.get_yaxis().labelpad = 15
     cb.set_label(r'RMSE [mW nm$^{-1}$ m$^{-2}$ ster$^{-1}$]', rotation=270)
