c25c1211f3302631a7d45ece1ab7419245ba37fa
voehl12 <veronika.oehl@gmx.de>
2022-09-16 14:18:56 +0200

    Comment and organize

--- fl_hyplant.py
+++ fl_hyplant.py
@@ -17,7 +17,10 @@ plt.rcParams.update({
     "font.serif": ["Computer Modern Roman"]})
 
 
+################### retrieval parameters ###############################
+
 ranges = [681,695,687]
+# pixel numbers of a green (pg) and non vegetated (pw) pixel
 pg = 2075*384+152#461714
 pw = 2036*384+117#831696
 
@@ -25,13 +28,34 @@ windowmin = ranges[0]
 windowmax = ranges[1]
 eval_wl = ranges[2]
 
-hyplant_res = results.retrieval_res('Hyplant','{:d}_{:d}'.format(pg,pw),windowmin,windowmax,eval_wl,'hyplant_res')
+polyorder = 2
 
+jmin = -2.0
+jmax = 1.0
+nlevels = 2048
+
+########################################################################
+
+############### data preparation #######################################
 
 wlorig, upsignalorig, referenceorig = prepare_input.hyplant(pg,pw,wlmin=660)
 wl, upsignal, reference = prepare_input.hyplant(pg,pw,wlmin=windowmin,wlmax=windowmax)
-
+hyplant_res = results.retrieval_res('Hyplant','{:d}_{:d}'.format(pg,pw),windowmin,windowmax,eval_wl,'hyplant_res')
 hyplant_res.init_wl(wl)
+
+########################################################################
+
+####################### SFM ###########################################
+
+sfmmin = np.argmin(np.fabs(wlorig-670))
+sfmmax = np.argmin(np.fabs(wlorig-780))
+sfmWL = wlorig[sfmmin:sfmmax]
+x,Fsfm,Rsfm,resnorm, exitflag, nfevas,sfmres = SFM.FLOX_SpecFit_6C(sfmWL,referenceorig[sfmmin:sfmmax],upsignalorig[sfmmin:sfmmax],[1,1],1.,wl,alg='trf')
+hyplant_res.Fsfm.spec = Fsfm
+
+########################################################################
+
+################### data visualization #################################
     
 rangefig, rangeax = plt.subplots()
 rangeax.plot(wlorig,upsignalorig)
@@ -43,29 +67,9 @@ figname1tex = 'HP_examplerange_{:d}{:d}_oens_{:d}_{:d}.tex'.format(windowmin,win
 tikzplotlib.save(figure=rangefig,filepath=figname1tex)
 rangefig.savefig(figname1)
 
+########################################################################
 
-
-polyorder = 2
-
-diurnal = []
-diurnalsfm = []
-diurnalR = []
-diurnalRsfm = []
-meanerrors = []
-diurnalRerrors = []
-diurnalFerrors = []
-diurnalappRerrors = []
-testjmin = -2.0
-testjmax = 1.0
-testnlevels = 2048
-
-resfig,(resax1,resax2) = plt.subplots(1,2,figsize=(10,6))
-scatfig, scatax = plt.subplots()
-#datfig, datax = plt.subplots()
-Fws = []
-Fsfms = []
-sfm_residuals = []
-
+############### initial guess and reflectance optimization #############
 
 appref = np.divide(upsignal,reference)
 nopeak_wl, nopeak_appref = prepare_input.rm_peak(wl,appref) 
@@ -77,27 +81,13 @@ if len(nopeak_appref) < len(wl):
     nopeak_wl = wl
 
 
-
 p_init = np.polyfit(nopeak_wl,nopeak_appref,polyorder)
 interp = np.poly1d(p_init)
 poly_R_init = interp(wl)
 
-newdecomp = wavelets.decomp(testjmin,testjmax,testnlevels)
+newdecomp = wavelets.decomp(jmin,jmax,nlevels)
 newdecomp.adjust_levels(upsignal)
 print(newdecomp.jmin,newdecomp.jmax,wavelets.get_wlscales(newdecomp.scales*(wl[1]-wl[0])))
-    
-
-
-newdecomp.calc_weights(upsignal)
-weights = newdecomp.weights
-
-sfmmin = np.argmin(np.fabs(wlorig-670))
-sfmmax = np.argmin(np.fabs(wlorig-780))
-sfmWL = wlorig[sfmmin:sfmmax]
-x,Fsfm,Rsfm,resnorm, exitflag, nfevas,sfmres = SFM.FLOX_SpecFit_6C(sfmWL,referenceorig[sfmmin:sfmmax],upsignalorig[sfmmin:sfmmax],[1,1],1.,wl,alg='trf')
-hyplant_res.Fsfm.spec = Fsfm
-sfm_residuals.append(sfmres)
-sunreference,_ = prepare_input.match_solspec(wl,0.3)
 
 coeffs = rpoly.optimize_coeffs(wl,reference,upsignal,p_init,newdecomp)
 polyrefls = []
@@ -109,59 +99,48 @@ for i,polycoef in enumerate(coeffs[0]):
     polyrefls.append(interp(wl))
     plt.plot(wl,interp(wl),color=cmap(i))
 
-
-
-
 polyrefls = np.array(polyrefls)
+
+############# weighting and final reflectance ##########################
+
+newdecomp.calc_weights(upsignal)
+weights = newdecomp.weights
+
 polyR, R_err = funcs.weighted_std(polyrefls,weights=weights,axis=0)
 hyplant_res.R = polyR
 
+########################################################################
 
+############# SIF extraction and evaluation #############################
 
+F_der = upsignal-polyR*reference
+hyplant_res.F.spec = F_der
 
+F_param = np.polyfit(wl,F_der,2)
+Finterp = np.poly1d(F_param)
+F_smooth = Finterp(wl)
+
+hyplant_res.F.evaluate_sif()
+hyplant_res.Fsfm.evaluate_sif()
+
+########################################################################
+
+####################### Plotting #######################################
 
 plt.plot(wl,polyR,color='tab:red',label=r'Wavelet Reflectance')
 plt.plot(wl,appref,color='tab:blue',label=r'Apparent Reflectance')
 
 plt.plot(wl,Rsfm,label=r'Spectral Fitting Method',color='limegreen')
 plt.xlim(windowmin,windowmax)
-#plt.ylim(0.3,0.6)
-
 plt.legend()
 
-
-
-F_der = upsignal-polyR*reference
-hyplant_res.F.spec = F_der
-
-
+resfig,(resax1,resax2) = plt.subplots(1,2,figsize=(10,6))
 resax1.plot(wl,polyR,color='forestgreen',linewidth=0.8,label='Reflectance')
 resax1.plot(wl,appref,'--',color='forestgreen',linewidth=0.8,label='Apparent Reflectance')
 resax2.plot(wl,F_der,color='tab:red',linewidth=0.8)
-
 resax2.plot(wl,Fsfm,'--',color='tab:red',linewidth=0.8)
-#datax.plot(wlorig,signalorig,color=cm(t),label=plottimes[t],linewidth=0.8)
-#datax.plot(wlorig,whitereferenceorig,color=cm(t),linewidth=0.8)
-    
-
-
-F_param = np.polyfit(wl,F_der,2)
-Finterp = np.poly1d(F_param)
-F_smooth = Finterp(wl)
-hyplant_res.F.evaluate_sif()
-diurnal.append(hyplant_res.F.spec_val)
-hyplant_res.Fsfm.evaluate_sif()
-diurnalsfm.append(hyplant_res.Fsfm.spec_val)
-
-Fws.append(F_der)
-Fsfms.append(Fsfm)
-
-diurnalR.append(np.median(polyR))
-diurnalRsfm.append(np.median(Rsfm))
-diurnalRerrors.append(np.median(R_err))
-
-
-
 
 plt.show()
+
+########################################################################
 
